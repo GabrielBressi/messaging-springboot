b4acc1b5e5c7b979446fb0aecb83bbe50ff2b1d1
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-09 16:46:11 -0300

    first commit

---
+++ src/main/java/br/com/gabriel/messagingspringboot/MessagingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.messagingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingSpringbootApplication.class, args);
+	}
+
+}
